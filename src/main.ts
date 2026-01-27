488a86fb0c4a91a9bf70ddd0ad5c6b673f9e5415
Rojon Ali <rojon009@gmail.com>
2026-01-27 11:48:21 +0600

    feat: Enhance global validation and CORS configuration in the NestJS application

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,23 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  // Global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // configure cors
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
