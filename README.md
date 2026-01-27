7314d6e419a6824fc125b273895941fe1a04c4ab
Rojon Ali <rojon@particularaudience.com>
2026-01-27 07:46:46 +0600

    feat: Initialize NestJS project with a basic user module and TypeORM database setup.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Plan
+
+- setup basic crud api
+- add auth
+- add database
+- handle proper success and error responses
+- add proper logging (grafana, open telemetry)
+- add proper monitoring (grafana, open telemetry)
+- add proper tracing (grafana, open telemetry)
+- add proper metrics (grafana, open telemetry)
+- add proper security (auth, rate limiting, etc)
+- add proper testing (unit, e2e, integration)
+- add proper documentation (swagger, etc)
+- add proper deployment (docker, k8s, etc)
---
+++ src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from 'src/users/entities/user.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(), // To load environment variables if needed
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Optional if you use env variables
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow<string>('DB_HOST'),
+        port: configService.getOrThrow<number>('DB_PORT'),
+        username: configService.getOrThrow<string>('DB_USERNAME'),
+        password: configService.getOrThrow<string>('DB_PASSWORD'),
+        database: configService.getOrThrow<string>('DB_NAME'),
+        entities: [User],
+        synchronize: true, // Disable in production
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule { }
